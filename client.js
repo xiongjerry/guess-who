d6f6b440bec6700be77a8a3ce1618cc1d85bc631
Jerry Xiong <xion0597@umn.edu>
2021-05-17 22:26:42 -0500

    revamp name generator,made events upon correct and wrong guesses

--- client.js
+++ client.js
@@ -1,6 +1,8 @@
 console.log('Here are all the available people:', people);
 
 
+
+
 $(document).ready(readyNow)
 
 function readyNow(){
@@ -8,18 +10,48 @@ function readyNow(){
 // function to display pictures
 displayProfiles();
 
+// function generating names
+randomName()
+
+// click listeners
+$('.portraits').on('click', '.pictures', handleClicks)
+
 }
 
 function displayProfiles(){
-    $('.portraits').append(`
-    <div>
-    <img src="https://github.com/christopher-black.png?size=250" alt="Profile image of Chris">
-    <img src="https://avatars.githubusercontent.com/u/17906158?v=4" alt="Profile image of Chris">
-    <img src="https://avatars.githubusercontent.com/u/11574995?v=4" alt="Profile image of Chris">
-    <img src="https://avatars.githubusercontent.com/u/5143491?v=4" alt="Profile image of Chris">
-    <img src="https://avatars.githubusercontent.com/u/5541481?v=4" alt="Profile image of Chris">
-    <img src="https://avatars.githubusercontent.com/u/1153371?v=4" alt="Profile image of Chris">
-    <img src="https://avatars.githubusercontent.com/u/10237149?v=4" alt="Profile image of Chris">
+    for (staff of people){
+        $('.portraits').append(`
+    <div class="pictures" data-name="${staff.name}" data-git="${staff.githubUsername}">
+    <img src="https://github.com/${staff.githubUsername}.png?size=250" alt="Profile image of ${staff.name}">    
     </div>
     `)
+    }
+}
+
+// random number generator 
+function randomNumber(min, max){
+     return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+// random name generator
+function randomName(){
+    return $('#name').text(`${people[randomNumber(0,6)].name}`);
+}
+
+
+
+function handleClicks(){
+    let picName = $(this).closest('div').data('name') // targets the name from portraits
+    console.log(picName);
+    let matchName = $('#name').text() // targets the name created from the random gen
+    console.log(matchName);
+    
+    if (picName == matchName) {
+        console.log('match!');
+        alert('NICE!!! You got a match, try again?')
+        randomName(); // resets upon correct guess
+    } else {
+        console.log('no match!');
+        alert('Error, Do Try Again!') 
+    }
 }
